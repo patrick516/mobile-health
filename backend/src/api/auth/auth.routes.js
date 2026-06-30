b273a6d01dd656bae0f079ea220718cf66ba6497
patrick516 <kulinjipatricks@gmail.com>
2026-06-30 13:25:16 +0200

    adding post to complete change pin route:

--- backend/src/api/auth/auth.routes.js
+++ backend/src/api/auth/auth.routes.js
@@ -15,6 +15,7 @@ router.post("/login", login);
 router.get("/me", authenticate, getMe);
 router.patch("/change-pin", authenticate, changePin);
 router.patch("/complete-pin-reset", completePinReset);
+router.post("/complete-pin-reset", completePinReset);
 router.post("/flag-lockout", flagLockout);
 router.patch("/unlock/:id", authenticate, unlockAccount);
 
